e41a658462ced8233f6ec37bccfd35e45c854d4a
e-taira <107139964+e-taira@users.noreply.github.com>
2022-12-22 10:43:31 -0600

    adding pacman loader

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 
 const About = () => {
 
@@ -16,49 +17,52 @@ const About = () => {
         }, []) 
 
     return (
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                    letterClass={letterClass}
-                    strArray={['A','b','o','u','t', ' ', 'm', 'e']}
-                    idx={15}
-                    />
-                </h1>
-                <p>
-                    first line of text
-                </p>
-                <p>
-                    second line of text
-                </p>
-                <p>
-                    final line of text 
-                </p>
-            </div>
+        <>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={['A','b','o','u','t', ' ', 'm', 'e']}
+                        idx={15}
+                        />
+                    </h1>
+                    <p>
+                        first line of text
+                    </p>
+                    <p>
+                        second line of text
+                    </p>
+                    <p>
+                        final line of text 
+                    </p>
+                </div>
 
-            <div className='stage-cube-cont'>
-                <div className='cubespinner'>
-                    <div className='face1'>
-                        <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-                    </div>
-                    <div className='face1'>
-                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-                    </div>
-                    <div className='face2'>
-                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                    </div>
-                    <div className='face3'>
-                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-                    </div>
-                    <div className='face4'>
-                        <FontAwesomeIcon icon={faJsSquare} color="#EFD812" />
-                    </div>
-                    <div className='face5 '>
-                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD812" />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 }
 

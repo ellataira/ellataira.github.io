acc5864b626aee2fcebfcd900f91cbc362a083ff
e-taira <107139964+e-taira@users.noreply.github.com>
2023-01-05 11:39:52 -0600

    sdf

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route  element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
         </Route>
     </Routes>
     </>
